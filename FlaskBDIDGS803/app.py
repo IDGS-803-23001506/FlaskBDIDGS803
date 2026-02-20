9b38d52f66d7fbe9157f59205cca2ca2ba2d9420
IDGS-80223001506 <mi.hernan672@gmail.com>
2026-02-19 21:13:39 -0600

    19/02/2026 - detalles

--- FlaskBDIDGS803/app.py
+++ FlaskBDIDGS803/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, flash, g, redirect, render_template, request, url_for
 from flask_wtf.csrf import CSRFProtect
 
+import forms
 from config import DevelopmentConfig
 from models import Alumnos, db
 
@@ -10,14 +11,37 @@ csrf=CSRFProtect()
 
 
 
-@app.route("/")
+@app.route("/", methods=['GET','POST'])
 @app.route("/index")
 def index():
-	return render_template("index.html")
+    create_form=forms.userForm(request.form)
+    alumno=Alumnos.query.all()
+    return render_template("index.html", form=create_form,alumno=alumno)
 
-@app.route("/Alumnos")
+
+
+@app.route("/Alumnos",  methods=['GET','POST'])
 def alumnos():
-    return render_template("Alumnos.html")
+    create_form=forms.userForm(request.form)
+    if request.method=="POST":
+        alum=Alumnos(nombre=create_form.nombre.data,
+                     aPaterno=create_form.apaterno.data,
+                     email=create_form.email.data)
+        db.session.add(alum)
+        db.session.commit()
+        return redirect(url_for('index'))
+    return render_template("Alumnos.html", form=create_form)
+
+@app.route("/detalles", methods=['GET','POST'])
+def detalles():
+    if request.method=='GET':
+        id=request.args.get('id')
+        alum1=db.session.query(Alumnos).filter(Alumnos.id==id).first()
+        id=request.args.get('id')
+        nombre=alum1.nombre
+        apaterno=alum1.aPaterno
+        email=alum1.email
+    return render_template("detalles.html", nombre=nombre,apaterno=apaterno,email=email)
 
 @app.errorhandler(404)
 def page_not_found(e):
